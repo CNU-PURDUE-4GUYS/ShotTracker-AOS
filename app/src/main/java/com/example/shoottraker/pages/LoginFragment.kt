acb5874ea3a0bc2c78aeecf99f1ccb5ca6e50360
Zack Murphy <zackm@rkdfamily.com>
2022-02-01 13:50:17 -0500

    Merged MAIN into zack-workspace

---
+++ app/src/main/java/com/example/shoottraker/pages/LoginFragment.kt
@@ -0,0 +1,11 @@
+package com.example.shoottraker.pages
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+    private var test = 0
+}
