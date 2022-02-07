06fa4238dcc458b4110ff1deffb47595616699a3
k906506 <k906506@naver.com>
2022-02-08 06:12:47 +0900

    [feat] Add camera functions

--- app/src/main/java/com/example/shoottraker/bottomMenu/NewShotFragment.kt
+++ app/src/main/java/com/example/shoottraker/bottomMenu/NewShotFragment.kt
@@ -1,54 +1,56 @@
 package com.example.shoottraker.bottomMenu
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.shoottraker.MainActivity
+import com.example.shoottraker.R
 import com.example.shoottraker.databinding.FragmentNewshotBinding
+import com.example.shoottraker.newShot.SetCameraFragment
+import com.google.android.material.snackbar.Snackbar
 
 class NewShotFragment : Fragment() {
     private lateinit var binding: FragmentNewshotBinding
     private lateinit var mainActivity: MainActivity
-
-    private var totalSet = 0
-    private var bulletPerSet = 0
+    private var initState = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Todo need to save Fragment state
+        if (savedInstanceState != null) {
+            initState = savedInstanceState.getBoolean("initState")
+        }
         binding = FragmentNewshotBinding.inflate(inflater, container, false)
+        mainActivity = activity as MainActivity
 
-//        initStartButton()
-//        clickStartButton()
+        setStartShotButton()
+        setTargetImageButton()
 
         return binding.root
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mainActivity = MainActivity()
+    // 카메라 세팅 설정
+    private fun setTargetImageButton() {
+        binding.setCameraButton.setOnClickListener {
+            binding.startShotButton.isEnabled = true
+            mainActivity.setAndSaveFragment(SetCameraFragment())
+        }
     }
 
-//    private fun initStartButton() {
-//        if (binding.totalSetEditText.text.isNotEmpty() && binding.totalShotEditText.text.isNotEmpty()) {
-//            binding.startButton.isEnabled = true
-//        }
-//    }
+    private fun setStartShotButton() {}
 
-//    private fun clickStartButton() {
-//        if (binding.totalSetEditText.text.isNotEmpty() && binding.totalShotEditText.text.isNotEmpty()) {
-//            binding.startButton.setOnClickListener {
-//                totalSet = binding.totalSetEditText.text.toString().toInt()
-//                bulletPerSet = binding.totalShotEditText.text.toString().toInt()
-//
-//                val intent = Intent(mainActivity, InShotActivity::class.java)
-//                startActivity(intent)
-//            }
-//        }
-//    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("initState", initState)
+    }
 }
