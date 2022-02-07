5c6c230aced2a347f3a8bdffad134bdcb89c2f8b
k906506 <k906506@naver.com>
2022-02-08 06:10:16 +0900

    [feat] Add get history function using Retrofit2

--- app/src/main/java/com/example/shoottraker/bottomMenu/HistoryFragment.kt
+++ app/src/main/java/com/example/shoottraker/bottomMenu/HistoryFragment.kt
@@ -1,43 +1,71 @@
 package com.example.shoottraker.bottomMenu
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.shoottraker.data.Shot
+import com.example.shoottraker.adapter.HistoryAdapter
 import com.example.shoottraker.databinding.FragmentHistoryBinding
-import com.example.shoottraker.history.DisplayBulletTraceAdapter
-import com.example.shoottraker.history.EachHistoryActivity
+import com.example.shoottraker.dto.HistoryDto
+import com.example.shoottraker.service.HistoryService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class HistoryFragment : Fragment() {
-    private lateinit var binding: FragmentHistoryBinding
+
+    private val binding by lazy {
+        FragmentHistoryBinding.inflate(layoutInflater)
+    }
+
+    private val adapter by lazy {
+        HistoryAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHistoryBinding.inflate(inflater, container, false)
 
-        initAdapter()
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(layoutInflater.context)
+
+        getHistory()
 
         return binding.root
     }
 
-    private fun setImages() {
-        // todo - connect Image API and set the imageUrl in the arrayOfData
-    }
+    private fun getHistory() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-    private fun initAdapter() {
-        val adapter = DisplayBulletTraceAdapter(itemClickedListener = {
-            val intent = Intent(activity, EachHistoryActivity::class.java)
-            startActivity(intent)
-        })
-        binding.recyclerView.layoutManager = LinearLayoutManager(layoutInflater.context)
-        binding.recyclerView.adapter = adapter
+        retrofit.create(HistoryService::class.java).also {
+            it.listHistories()
+                .enqueue(object : Callback<HistoryDto> {
+                    override fun onResponse(
+                        call: Call<HistoryDto>,
+                        response: Response<HistoryDto>
+                    ) {
+                        if (response.isSuccessful.not()) {
+                            // 예외 처리
+                        }
+                        response.body()?.let { historyDto ->
+                            adapter.submitList(historyDto.histories)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<HistoryDto>, t: Throwable) {
+                        // 예외 처리
+                    }
+                }
+                )
+        }
     }
 }
